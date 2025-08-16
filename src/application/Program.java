afede97b573a17a1edce6e0b47c0a211c9c3c13f
Hudson Amorim <hudson-eng-software@gmail.com>
2025-08-16 01:30:05 -0300

    Java Estoque Program

---
+++ src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+        System.out.println();
+        System.out.println("Product Data: " + product);
+        System.out.println();
+        System.out.print(" Enter the number of products to be added  in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update Data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.printf();
+        System.out.println("Update Data: " + product);
+        sc.close();
+
+    }
+}
